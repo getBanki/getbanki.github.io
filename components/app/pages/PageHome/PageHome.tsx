f945fa4837ce50049c252f9b18697192fa185681
getBanki <138777187+getBanki@users.noreply.github.com>
2023-07-06 23:36:47 +0600

    Add files via upload

--- components/app/pages/PageHome/PageHome.tsx
+++ components/app/pages/PageHome/PageHome.tsx
@@ -5,6 +5,7 @@ import './PageHome.scss'
 import Image from "next/image";
 import {Rubik} from "next/font/google";
 import gif from '../../../../public/X11D.gif'
+import prew from '../../../../public/prew.svg'
 
 const rubik = Rubik({
     weight: '700',
@@ -19,18 +20,32 @@ export const PageHome = observer(() => {
 
 
         return (
-            <div className='page-home'>
-               <div className='flex'>
+            <div className='page-home animation'>
+                <div className='flex'>
 
-                   <div className='page-home__text'>
-                       <div className={`page-home__slogan ${rubik.className}`}> Экономьте время и деньги</div>
-                       <div className={`page-home__desc ${rubikLight.className}`}> с нашим сервисом подбора займов, кредитов и кредитных карт</div>
-                   </div>
-                   <div className='page-home__img'>
-                       {/*<Image unoptimized={true} src={gif} alt={'лучшие займы'} height={512} width={512}/>*/}
+                    <div className='page-home__text'>
+                        <div className={`page-home__slogan ${rubik.className}`}> Я экономлю время и деньги</div>
+                        <div className={`page-home__desc ${rubikLight.className}`}> с сервисом подбора займов, кредитов и
+                            кредитных карт
+                        </div>
+                        <div className={`page-home__desc ${rubikLight.className}`}>
+                            от
+                            <span
+                                className={`${rubik.className} ml-3`}>
+                             Get
+                            </span>
+                            <span style={{color: '#3E68FF'}}
+                                  className={rubik.className}>
+                                Banki
+                            </span>
+                        </div>
+                        <a href={'/loans'}  className="btn btn-primary page-home__btn mt-6">Начать!</a>
+                    </div>
+                    <div className='page-home__img'>
+                        <Image unoptimized={true} src={prew} alt={'лучшие займы'} height={600} width={800}/>
 
-                   </div>
-               </div>
+                    </div>
+                </div>
             </div>
         )
     }
