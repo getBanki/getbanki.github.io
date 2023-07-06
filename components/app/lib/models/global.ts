55284a258454d74d67ca3f3c241917600ede5e28
getBanki <138777187+getBanki@users.noreply.github.com>
2023-07-06 16:53:31 +0600

    Add files via upload

--- components/app/lib/models/global.ts
+++ components/app/lib/models/global.ts
@@ -1,20 +1,17 @@
 
 
-import { OptionSelect } from './option-select'
+
 import { IInit, Init } from './init'
-import {IDataApi} from "../../../app/lib/models/dataApi";
+
 
 
 export type IGlobal = IInit
 
 export class Global<I, T> extends Init<I, T> implements IGlobal {
-    constructor(obj: IDataApi) {
+    constructor(obj: any) {
         // @ts-ignore
         super(obj)
     }
 
-    toSelectOption(nameFiled: string, labelField = nameFiled, icon = ''): OptionSelect {
-        // @ts-ignore
-        return new OptionSelect({ label: this[labelField], value: this[nameFiled], icon: icon })
-    }
+
 }
