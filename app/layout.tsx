133cd2569fda52658df27661de4d8821edaef0e3
getBanki <138777187+getBanki@users.noreply.github.com>
2023-07-11 19:34:59 +0600

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({children}: IProps) {
     return (
         <html lang="ru" suppressHydrationWarning={true} data-theme="light">
         <head>
-            <link rel="manifest" href="/manifest.json" />
             <link rel="shortcut icon" href="/logo.svg" type="image/x-icon"/>
             <title>GETBANKI</title>
         </head>
--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-});
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
 
-module.exports = withPWA({
-    reactStrictMode: true,
-});
+module.exports = nextConfig
