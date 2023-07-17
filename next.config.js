931ea1d883a6646bcb06db4ddbdc4a79991810b3
getBanki <138777187+getBanki@users.noreply.github.com>
2023-07-17 15:26:14 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-    
-    const nextConfig = {
-      reactStrictMode: true,
-      
-    }
-        
-    module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
